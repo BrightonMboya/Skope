61be8e562e46b78fd7aa1b194dc65c4b0d739d59
BrightonMboya <b.mboya@alustudent.com>
2022-05-13 23:08:17 +0200

    did nothing

--- Spoke/spoke/src/Components/NavBar/HeroImages.jsx
+++ Spoke/spoke/src/Components/NavBar/HeroImages.jsx
@@ -9,9 +9,9 @@ const HeroImages = () => {
           <img src={img1} alt="" className="w-40 h-40 rounded-full shadow-2xl md:w-60 h-60"/>
           <img src={img2} alt="" className="w-20 h-20 rounded-full shadow-2xl md:w-30" />
       </div>
-      {/* <div className="ml-[40vh] hidden">
+       <div className="ml-[40vh] hidden">
           <img src={img3} alt="img3" className="w-20 h-20 rounded-full shadow-2xl" />
-      </div> */}
+      </div>
   </div>;
 };
 
